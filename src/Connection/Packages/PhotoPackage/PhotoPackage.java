4d9790bd3507985358a7b04c69040e0d7bd7432a
Ly659 <2011202398@qq.com>
2026-03-29 10:30:23 +0800

    编写PhotoPackage封装 2026.3.29

--- src/Connection/Packages/PhotoPackage/PhotoPackage.java
+++ src/Connection/Packages/PhotoPackage/PhotoPackage.java
@@ -4,71 +4,104 @@ import java.io.Serializable;
 
 /**
  * 用于封装图片数据的Package。此类可以被序列化，通过Socket传输。
+ * 此封装包含的信息有：
+ * 文件数据
+ * 文件路径
+ * 照片尺寸
+ *
+ * @param photoData 文件内容数据
+ * @param filePath  存储图片信息 文件路径（\Storage\emulated\0\为根目录）
+ * @param photoType 文件类型
+ * @param fileSize  文件大小（单位：MB）
+ * @param photoSize 图片尺寸
  */
-public class PhotoPackage implements Serializable {
-    private final byte[] photoData;       // 文件内容数据
-
-    // 存储图片信息
-    private String fileName;        // 文件名
-    private final PhotoTypes photoType;   // 文件类型
-    private final PhotoSize photoSize;    // 图片尺寸
+public record PhotoPackage(byte[] photoData, String filePath, int fileSize, PhotoTypes photoType,
+                           PhotoSize photoSize) implements Serializable {
     //……//
 
     /**
      * 初始化封装，需要指定图片的数据和文件名
      * 此构造用于存储和传输缩略图。
+     *
      * @param photoData 图片数据
-     * @param fileName 文件名
+     * @param filePath  文件名
+     * @param fileSize  文件大小（单位：MB）
+     * @param photoType 文件格式（常量）
      * @param photoSize 原图的尺寸信息
      */
-    public PhotoPackage(byte[] photoData, String fileName, PhotoTypes type, PhotoSize photoSize) {
-        this.photoData = photoData;
-        this.fileName = fileName;
-        this.photoType = type;
-        this.photoSize = photoSize;
+    public PhotoPackage {
     }
 
+    // 暂时不用了。发送原图和发送其他文件一样，直接使用文件流了。
+//    /**
+//     * 初始化封装，仅需指定图片数据。
+//     * 此构造用于存储和传输原图。
+//     * @param photoData 图片数据
+//     */
+//    public PhotoPackage(byte[] photoData) {
+//        photoSize = null;
+//        photoType = null;
+//        fileSize = -1;
+//        this.photoData = photoData;
+//    }
+
+
     /**
-     * 初始化封装，仅需指定图片数据。
-     * 此构造用于存储和传输原图。
-     * @param photoData 图片数据
+     * 获取图片的文件路径。
+     *
+     * @return 文件路径
      */
-    public PhotoPackage(byte[] photoData) {
-        photoSize = null;
-        photoType = null;
-        this.photoData = photoData;
+    @Override
+    public String filePath() {
+        return filePath;
     }
 
-
     /**
-     * 获取图片的文件名。
+     * 获取文件名。
+     *
      * @return 文件名
      */
     public String getFileName() {
-        return fileName;
+        return filePath.substring(filePath.lastIndexOf("\\"));
     }
 
     /**
      * 获取图片的数据。
+     *
      * @return 图片数据
      */
-    public byte[] getPhotoData() {
+    @Override
+    public byte[] photoData() {
         return photoData;
     }
 
     /**
      * 获取图片的尺寸信息。
+     *
      * @return 储存信息的封装类（包含宽Width和高Height）
      */
-    public PhotoSize getPhotoSize() {
+    @Override
+    public PhotoSize photoSize() {
         return photoSize;
     }
 
     /**
      * 获取文件的格式类型。
+     *
      * @return 图片的文件格式（一个常量）
      */
-    public PhotoTypes getPhotoType() {
+    @Override
+    public PhotoTypes photoType() {
         return photoType;
+    }
+
+    /**
+     * 获取文件的大小信息。
+     *
+     * @return 文件大小（单位：MB）
+     */
+    @Override
+    public int fileSize() {
+        return fileSize;
     }
 }
